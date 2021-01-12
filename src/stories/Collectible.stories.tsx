6680d9e28a5c3dd874992d5eec9717c2e4c06562
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-01-12 13:54:29 +0100

    [FEAT] Added new story for collectible component

---
+++ src/stories/Collectible.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Collectible, { CollectibleParams } from '../components/Collectible';
+
+export default {
+    title: 'Example/Collectible',
+    component: Collectible
+} as Meta;
+
+const Template: Story<CollectibleParams> = (args) => <Collectible
+    {...args}
+    name="Collectible 1"
+    count={12}
+    collection="collection1" />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    primary: true,
+    label: 'Collectible'
+};
